76618b3be65a723dd3ca220c327a078eee453446
sabinehamdoun <sabine.hamdoun@lau.edu>
2023-10-05 14:57:47 +0300

    s

--- src/components/header.js
+++ src/components/header.js
@@ -12,14 +12,14 @@ const Header = () => {
             <a href="#home"><img src={"/assets/logo-bg.png"} alt="logo" className="h-20"/></a>
           </div>
           <div className="flex justify-end items-center ml-auto">
-            <ul className='list-none flex gap-4 sm:gap-12 uppercase pr-9 sm:mr-14'>
-              <li className="hover:bg-gray-400 hover:rounded-md hover:px-4">
+            <ul className='list-none flex gap-1 sm:gap-9 uppercase mr-4 sm:mr-14'>
+              <li className="hover:bg-gray-400 rounded-md px-4 py-1">
                 <Link href="/home">Home</Link>
               </li>
-              <li className="hover:bg-gray-400 hover:rounded-md hover:px-4">
+              <li className="hover:bg-gray-400 rounded-md px-4 py-1">
                 <Link href="/about">About</Link>
               </li>
-              <li className="hover:bg-gray-400 hover:rounded-md hover:px-4">
+              <li className="hover:bg-gray-400 rounded-md px-4 py-1"> 
                 <Link href="/contact">Contact</Link>
               </li>
             </ul>
