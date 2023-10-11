d7919e6b800d70828a1a0745a420b100e278bcbb
sabinehamdoun <sabine.hamdoun@lau.edu>
2023-10-11 15:27:33 +0300

    s

--- src/components/accordion.js
+++ src/components/accordion.js
@@ -1,73 +1,36 @@
 import React, { useState } from "react";
-import {
-  Accordion,
-  AccordionHeader,
-  AccordionBody,
-} from "@material-tailwind/react";
 import { AiOutlinePlus } from "react-icons/ai";
 import { AiOutlineMinus } from "react-icons/ai";
 
-function DefaultAccordion() {
-  const [open, setOpen] = useState(false);
+function AccordionItem({ title, content, index }) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleToggle = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const backgroundColor = index % 2 === 0 ? "bg-[#f0f0f0]" : "bg-[#f0f0f031]";
 
   return (
-    <Accordion open={open} className="bg-[#f0f0f0] px-3 py-1 rounded-md">
-      <AccordionHeader onClick={() => setOpen(!open)} className="text-lg">
-        How can I learn a new programming language?
-        {open ? (
+    <div className={`${backgroundColor} px-4 py-4 rounded-md`}>
+      <div className={"text-lg flex items-center"} onClick={handleToggle}>
+        {title}
+        {isOpen ? (
           <AiOutlineMinus className="ml-auto" />
         ) : (
           <AiOutlinePlus className="ml-auto" />
         )}
-      </AccordionHeader>
-      <AccordionBody className={`py-3 ${open ? "block" : "hidden"}`}>
-        Learning a new programming language involves studying its syntax,
-        practicing coding, and working on projects to apply your knowledge.
-        Online resources and courses can be helpful.
-      </AccordionBody>
-    </Accordion>
+      </div>
+      {isOpen && (
+        <div>
+          <div>
+            <hr className="border-1 mt-3 border-[#1c21233d]" />
+          </div>
+          <div className="text-sm py-3">{content}</div>
+        </div>
+      )}
+    </div>
   );
 }
 
-export default DefaultAccordion;
-
-//       <Accordion open={open === 2} className="bg-[#f0f0f031] px-3 py-1 rounded-md">
-//         <AccordionHeader onClick={() => handleOpen(2)} className="text-lg">
-//           What is version control and why is it important for software
-//           development?
-//           {open === 2 ? (
-//             <AiOutlineMinus className="ml-auto" />
-//           ) : (
-//             <AiOutlinePlus className="ml-auto" />
-//           )}
-//         </AccordionHeader>
-//         <AccordionBody className={`py-3 ${open === 2 ? "block" : "hidden"}`}>
-//           Version control is a system that tracks changes to a codebase over
-//           time. It is important for software development because it allows
-//           multiple developers to collaborate, revert to previous versions, and
-//           maintain a history of code changes, ensuring code stability and
-//           accountability.
-//         </AccordionBody>
-//       </Accordion>
-
-//       <Accordion open={open === 3} className="bg-[#f0f0f0] px-3 py-1 rounded-md">
-//         <AccordionHeader onClick={() => handleOpen(3)} className="text-lg">
-//           What is object-oriented programming (OOP)?
-//           {open === 3 ? (
-//             <AiOutlineMinus className="ml-auto" />
-//           ) : (
-//             <AiOutlinePlus className="ml-auto" />
-//           )}
-//         </AccordionHeader>
-//         <AccordionBody className={`py-3 ${open === 3 ? "block" : "hidden"}`}>
-//           Object-oriented programming is a programming paradigm that uses
-//           objects, which are instances of classes, to represent and manipulate
-//           data. It focuses on concepts like encapsulation, inheritance, and
-//           polymorphism for building modular and maintainable code.
-//         </AccordionBody>
-//       </Accordion>
-//     </div>
-//   );
-// }
-
-// export default DefaultAccordion;
+export default AccordionItem;
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,3 +1,4 @@
+import AccordionItem from "@component/components/accordion";
 import DefaultAccordion from "@component/components/accordion";
 import Banner from "@component/components/banner";
 import Footer from "@component/components/footer";
@@ -7,6 +8,25 @@ import Services from "@component/components/services";
 import React from "react";
 
 const HomePage = () => {
+  const accordionItems = [
+    {
+      title: "How can I learn a new programming language?",
+      content:
+        "Learning a new programming language involves studying its syntax, practicing coding, and working on projects to apply your knowledge. Online resources and courses can be helpful.",
+    },
+    {
+      title:
+        "What is version control and why is it important for software development?",
+      content:
+        "Version control is a system that tracks changes to a codebase over time. It is important for software development because it allows multiple developers to collaborate, revert to previous versions, and maintain a history of code changes, ensuring code stability and accountability.",
+    },
+    {
+      title: "What is object-oriented programming (OOP)?",
+      content:
+        "Object-oriented programming is a programming paradigm that uses objects, which are instances of classes, to represent and manipulate data. It focuses on concepts like encapsulation, inheritance, and polymorphism for building modular and maintainable code.",
+    },
+  ];
+
   return (
     <>
       <Header />
@@ -20,8 +40,19 @@ const HomePage = () => {
         <Form />
       </div>
       <div className="p-14">
-        <h2 className="text-2xl text-center mb-3">Frequently Asked Questions</h2>
-        <DefaultAccordion />
+        <h2 className="text-2xl text-center mb-3">
+          Frequently Asked Questions
+        </h2>
+        <div className="w-[100%] max-w-screen-md py-1 sm:p-4 mx-auto">
+          {accordionItems.map((item, index) => (
+            <AccordionItem
+              key={index}
+              title={item.title}
+              content={item.content}
+              index={index}
+            />
+          ))}
+        </div>
       </div>
       <Footer />
     </>
